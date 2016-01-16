8a9b5c766f44527a12a052829171353380ac11b9
Jason Ramirez <ramirez676@gmail.com>
2016-01-16 10:41:55 -0500

    Add works show view
    * Add Tinysplash show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "welcome#index"
+
+  get "works/tinysplash", to: "works#tinysplash"
 end
--- spec/features/guest_views_works_spec.rb
+++ spec/features/guest_views_works_spec.rb
@@ -1,11 +1,21 @@
 require "rails_helper"
 
 RSpec.feature "Guest views works" do
-  context "at the root domain" do
-    it "shows the works" do
+  context "on the index page" do
+    it "shows the thumbnail" do
       visit "/"
 
       expect(page).to have_css "img.work"
+    end
+  end
+
+  context "on the show page" do
+    it "shows the details" do
+      visit "/"
+
+      click_link "tinysplash"
+
+      expect(page).to have_text "Tinysplash"
     end
   end
 end
