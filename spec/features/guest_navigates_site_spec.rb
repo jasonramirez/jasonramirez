039846f4062db9170a00dafd5b68a6815f40d55c
Jason Ramirez <ramirez676@gmail.com>
2016-01-16 10:52:42 -0500

    Add works link to navigation

---
+++ spec/features/guest_navigates_site_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.feature "Guest navigates site" do
+  describe "by clicking home" do
+    it "take the user to the landing page" do
+      visit root_path
+
+      click_link "tinysplash"
+      click_link t("navigation.works")
+
+      expect(page).to have_css "img.work"
+    end
+  end
+end
