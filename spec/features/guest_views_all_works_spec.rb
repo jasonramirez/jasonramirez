b54f7b51e52ee81c462d785b9f44efc906dc034f
jasonramirez <jason@thoughtbot.com>
2016-02-14 12:04:13 -0500

    Contain navigation content and add link to home

--- spec/features/guest_views_all_works_spec.rb
+++ spec/features/guest_views_all_works_spec.rb
@@ -5,8 +5,21 @@ RSpec.feature "Guest views all works" do
     it "shows the thumbnail" do
       visit "/"
 
-      expect(page).to have_css "li.tinysplash"
-      expect(page).to have_css "li.piggy"
+      works.each do |work|
+        expect(page).to have_css "##{work}"
+      end
     end
+  end
+
+  def works
+    [
+      "frida_and_fred",
+      "ouch",
+      "penner",
+      "piggy",
+      "project_underdog",
+      "repor",
+      "tinysplash",
+    ]
   end
 end
