99fa90ef8f96af0ac74b078c868371c76c707552
jasonramirez <jason@thoughtbot.com>
2016-02-14 16:44:35 -0500

    Add process to site

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "welcome#index"
 
+  get "process", to: "process#show"
   get "works/piggy", to: "works#piggy"
   get "works/tinysplash", to: "works#tinysplash"
 end
--- spec/features/guest_navigates_site_spec.rb
+++ spec/features/guest_navigates_site_spec.rb
@@ -1,14 +1,37 @@
 require "rails_helper"
 
 RSpec.feature "Guest navigates site" do
-  describe "by clicking home" do
-    it "take the user to the landing page" do
-      visit root_path
+  context "from the landing page" do
+    describe "clicking a project" do
+      it "takes you to that project's page" do
+        visit root_path
 
-      click_link "tinysplash"
-      click_link t("navigation.works")
+        click_link "piggy"
 
-      expect(page).to have_css "ul.works"
+        expect(page).to have_css "body.works-piggy"
+      end
+    end
+
+    describe "clicking the process link in the nav" do
+      it "takes you to that process page" do
+        visit root_path
+
+        click_link "process"
+
+        expect(page).to have_text t("process.show.title")
+      end
+    end
+  end
+
+  context "from a projects page" do
+    describe "clicking to home button" do
+      it "takes you to the home page" do
+        visit works_piggy_path
+
+        click_link t("navigation.works")
+
+        expect(page).to have_css "body.welcome"
+      end
     end
   end
 end
