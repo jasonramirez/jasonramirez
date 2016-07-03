cfaba005a3ae7057e3fbe71f530b9d1467e10360
Jason Ramirez <jason@jasonramirez.com>
2016-07-03 10:09:42 -0400

    Work footer refactor
    * Add test coverage to the footer navigation
    * Better component naming for the work footer navigation

---
+++ spec/features/guest_navigates_works_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.feature "Guest navigations works" do
+  context "from the footer navigation of the works detail page" do
+    describe "the next button" do
+      it "goes to the next work" do
+        visit works_piggy_path
+
+        within ".work-footer" do
+          click_link "Next"
+        end
+
+        expect(page).to have_text "Penner"
+      end
+    end
+
+    describe "the previous button" do
+      it "goes to the previous work" do
+        visit works_piggy_path
+
+        within ".work-footer" do
+          click_link "Previous"
+        end
+
+        expect(page).to have_text "Tinysplash"
+      end
+    end
+  end
+end
