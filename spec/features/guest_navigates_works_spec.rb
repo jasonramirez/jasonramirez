c5922f0f53a8275d125e810bcca0bbb2e221012d
Jason Ramirez <jason@jasonramirez.com>
2016-07-04 13:24:07 -0400

    Add work page navigation

--- spec/features/guest_navigates_works_spec.rb
+++ spec/features/guest_navigates_works_spec.rb
@@ -1,6 +1,28 @@
 require "rails_helper"
 
 RSpec.feature "Guest navigations works" do
+  context "from the hero navigation of the works detail page" do
+    describe "the next button" do
+      it "goes to the next work" do
+        visit works_piggy_path
+
+        page.first(".work-page-navigation--left").click
+
+        expect(page).to have_text "Penner"
+      end
+    end
+
+    describe "the previous button" do
+      it "goes to the previous work" do
+        visit works_piggy_path
+
+        page.first(".work-page-navigation--right").click
+
+        expect(page).to have_text "Tinysplash"
+      end
+    end
+  end
+
   context "from the footer navigation of the works detail page" do
     describe "the next button" do
       it "goes to the next work" do
