0ea683e2d06907fc48f22d40a6f9defe9fb549be
Jason Ramirez <jason@jasonramirez.com>
2016-08-14 20:28:30 -0400

    Remove blank padding from day of post date

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
 
   def pretty_published_date
-    published_date.strftime("%-m/%e/%y")
+    published_date.strftime("%-m/%-d/%y")
   end
 end
