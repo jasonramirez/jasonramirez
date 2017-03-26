d2e9b57adf85cc15c559c94724a96266cf7cf163
Jason Ramirez <jason@jasonramirez.com>
2017-03-26 18:05:36 -0400

    Remove test coverage for hero navigation for case studies

--- spec/features/guest_navigates_case_studies_spec.rb
+++ spec/features/guest_navigates_case_studies_spec.rb
@@ -1,28 +1,6 @@
 require "rails_helper"
 
 RSpec.feature "Guest navigations case studies" do
-  context "from the hero navigation of the case studies detail page" do
-    describe "the next button" do
-      it "goes to the next work" do
-        visit_piggy
-
-        page.first(".case-study-page-navigation--left").click
-
-        expect(page).to have_text "Penner"
-      end
-    end
-
-    describe "the previous button" do
-      it "goes to the previous work" do
-        visit_piggy
-
-        page.first(".case-study-page-navigation--right").click
-
-        expect(page).to have_text "Tinysplash"
-      end
-    end
-  end
-
   context "from the footer navigation of the case studies detail page" do
     describe "the next button" do
       it "goes to the next work" do
