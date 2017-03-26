bebdaf6a99a38a92d0393ca09abac91d9215aaef
Jason Ramirez <jason@jasonramirez.com>
2017-03-26 17:55:25 -0400

    Add test coverage for posts having a long title

--- spec/features/guest_views_posts_spec.rb
+++ spec/features/guest_views_posts_spec.rb
@@ -18,4 +18,13 @@ RSpec.feature "Guest views posts" do
     expect(page).to have_text post_one.title
     expect(page).to have_text post_two.title
   end
+
+  scenario "with a long title" do
+    post_one = create(:post, title: "ShortTitle", long_title: "LongerTitle")
+
+    visit posts_path
+
+    expect(page).to have_text post_one.long_title
+    expect(page).to_not have_text post_one.title
+  end
 end
