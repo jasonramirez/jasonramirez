c97bf0e5d50aefce8e52c2753bb222c8fdea9e0e
Jason Ramirez <jason@thoughtbot.com>
2017-03-26 22:23:41 -0400

    Improve spacing of post index in admin view

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,12 @@ class Post < ActiveRecord::Base
   def pretty_published_date
     published_date.strftime("%-m/%-d/%y")
   end
+
+  def published_state_class
+    published || "not-published"
+  end
+
+  def published_state
+    published ? "Published" : "Not Published"
+  end
 end
