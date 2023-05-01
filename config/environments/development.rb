a382195b86e5a41d80bd72c905ac34c4a5ad274e
Jason Ramirez <jason@jasonramirez.com>
2023-04-30 21:43:10 -0700

    Remove unsupported methods
    Remove config.action_view.raise_on_missing_translations = true from
    environment files.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,6 @@ Rails.application.configure do
   config.assets.debug = true
   config.assets.digest = true
   config.assets.raise_runtime_errors = true
-  config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: "localhost:3000" }
   config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
 end
