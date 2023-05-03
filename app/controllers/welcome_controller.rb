54e5db136cb19c2a75e00b32e2fd5725d8b2fee4
Jason Ramirez <jason@jasonramirez.com>
2023-05-02 18:23:59 -0700

    Add history to the home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,3 @@
 class WelcomeController < ApplicationController
+  include HistoryHelper
 end
---
+++ app/helpers/history_helper.rb
@@ -0,0 +1,122 @@
+module HistoryHelper
+  def history_item_list
+    history_list_array = Array.new()
+
+    return history_list_array = [
+      [
+        "image" => "dropbox-logo.png",
+        "title" => "Design Director",
+        "date" => "January 2022",
+        "organization" => "Dropbox"
+      ],
+      [
+        "image" => "dad.png",
+        "title" => "Dad",
+        "date" => "May 2020",
+        "organization" => "Life"
+      ],
+      [
+        "image" => "dropbox-logo.png",
+        "title" => "Senior Product Design Manager",
+        "date" => "October 2018",
+        "organization" => "Dropbox"
+      ],
+      [
+        "image" => "san-franciscan.png",
+        "title" => "San Franciscan",
+        "date" => "October 2018",
+        "organization" => "Life"
+      ],
+      [
+        "image" => "husband.png",
+        "title" => "Husband",
+        "date" => "October 2018",
+        "organization" => "Life"
+      ],
+      [
+        "image" => "thoughtbot-logo.png",
+        "title" => "Design Director",
+        "date" => "June 2015",
+        "organization" => "thoughtbot"
+      ],
+      [
+        "image" => "thoughtbot-logo.png",
+        "title" => "Product Design Manager",
+        "date" => "June 2013",
+        "organization" => "thoughtbot"
+      ],
+      [
+        "image" => "thoughtbot-logo.png",
+        "title" => "Product Designer",
+        "date" => "June 2013",
+        "organization" => "thoughtbot"
+      ],
+      [
+        "image" => "we-ate-the-web-logo.png",
+        "title" => "Founder, Product Designer",
+        "date" => "January 2011",
+        "organization" => "We Ate The Web"
+      ],
+      [
+        "image" => "the-print-collective-logo.png",
+        "title" => "Founder, Designer",
+        "date" => "January 2008",
+        "organization" => "The Print Collective"
+      ],
+      [
+        "image" => "parsons-logo.png",
+        "title" => "Learner",
+        "date" => "June 2007",
+        "organization" => "Parsons"
+      ],
+      [
+        "image" => "new-yorker.png",
+        "title" => "New Yorker",
+        "date" => "June 2007",
+        "organization" => "Life"
+      ],
+      [
+        "image" => "the-peak-agency-logo.png",
+        "title" => "Founder, Director",
+        "date" => "September 2002",
+        "organization" => "The Peak Agency"
+      ],
+      [
+        "image" => "des-moineser.png",
+        "title" => "Des Moineser",
+        "date" => "August 2002",
+        "organization" => "Life"
+      ],
+      [
+        "image" => "u-of-a-logo.png",
+        "title" => "Learner",
+        "date" => "August 2001",
+        "organization" => "Life"
+      ],
+      [
+        "image" => "tucsonian.png",
+        "title" => "Tucsonian",
+        "date" => "August 2001",
+        "organization" => "Life"
+      ],
+      [
+        "image" => "madisonian.png",
+        "title" => "Madisonian",
+        "date" => "June 1988",
+        "organization" => "Life"
+      ],
+      [
+        "image" => "chicagoan.png",
+        "title" => "Chicagoan",
+        "date" => "October 1982",
+        "organization" => "Life"
+      ],
+      [
+        "image" => "human.png",
+        "title" => "Human",
+        "date" => "October 1982",
+        "organization" => "Life"
+      ]
+    ]
+  end
+end
