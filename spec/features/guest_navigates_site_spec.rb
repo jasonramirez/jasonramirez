90752eb675b2b5d3be701d6bc8588a622417a35c
Jason Ramirez <jason@jasonramirez.com>
2023-05-16 14:43:43 -0700

    Update site navigation testing

--- spec/features/guest_navigates_site_spec.rb
+++ spec/features/guest_navigates_site_spec.rb
@@ -1,28 +1,67 @@
 require "rails_helper"
 
 RSpec.feature "Guest navigates site" do
-  context "from the landing page" do
-    describe "clicking a project" do
-      it "takes you to that project's page" do
+  context "from the header navigation" do
+    describe "clicking on work" do
+      it "takes you to the list of case studies" do
         visit root_path
 
-        page.find(:xpath, ".//a[@href='case_studies/piggy']").click
+        within ".site-header" do
+          click_link t("navigation.case_studies")
+        end
 
-        expect(page).to have_text "Piggy"
+        expect(page).to have_text "Mayo Clinic"
+      end
+    end
+
+    describe "clicking on posts" do
+      it "takes you to the list of posts" do
+        post = create(:post, title: "Post One", published: true)
+        visit root_path
+
+        within ".site-header" do
+          click_link t("navigation.posts")
+        end
+
+        expect(page).to have_text post.title
+      end
+    end
+
+    describe "clicking on principles" do
+      it "takes you to my design principles" do
+        visit root_path
+
+        within ".site-header" do
+          click_link t("navigation.principles")
+        end
+
+        expect(page).to have_text "design principles"
+      end
+    end
+
+    describe "clicking on values" do
+      it "takes you to my design values" do
+        visit root_path
+
+        within ".site-header" do
+          click_link t("navigation.values")
+        end
+
+        expect(page).to have_text "design values"
       end
     end
   end
 
-  context "from a projects page" do
-    describe "clicking to home button" do
-      it "takes you to the home page" do
-        visit "case_studies/piggy"
+  context "from the footer navigation" do
+    describe "clicking on follow" do
+      it "takes you to the follow page" do
+        visit root_path
 
-        within ".site-header" do
-          click_link t("navigation.home")
+        within ".site-footer" do
+          click_link t("navigation.follow")
         end
 
-        expect(page).to have_css "body.welcome"
+        expect(page).to have_text t("followers.new.sign_up")
       end
     end
   end
