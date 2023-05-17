2395ad83f6a5c760e1b9dcd2bd80235ee9a56319
Jason Ramirez <jason@jasonramirez.com>
2023-05-16 17:36:25 -0700

    Update tests for viewing protected case studies

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,17 +1,19 @@
 Rails.application.configure do
-  config.assets.raise_runtime_errors = true
-  config.cache_classes = false
-  config.eager_load = false
-  config.log_level = :warn
-  config.serve_static_files   = true
-  config.static_cache_control = 'public, max-age=3600'
-  config.consider_all_requests_local       = true
+  config.action_controller.allow_forgery_protection = false
   config.action_controller.perform_caching = false
   config.action_dispatch.show_exceptions = false
-  config.action_controller.allow_forgery_protection = false
-  config.action_mailer.delivery_method = :test
-  config.active_support.test_order = :random
-  config.active_support.deprecation = :stderr
   config.action_mailer.default_url_options = { host: "www.example.com" }
+  config.action_mailer.delivery_method = :test
   config.active_job.queue_adapter = :inline
+  config.active_record.legacy_connection_handling = false
+  config.active_support.deprecation = :stderr
+  config.active_support.test_order = :random
+  config.assets.raise_runtime_errors = true
+  config.assets.unknown_asset_fallback = true
+  config.cache_classes = false
+  config.consider_all_requests_local = true
+  config.eager_load = false
+  config.log_level = :warn
+  config.serve_static_files = true
+  config.static_cache_control = 'public, max-age=3600'
 end
--- spec/features/guest_views_protected_case_studies_spec.rb
+++ spec/features/guest_views_protected_case_studies_spec.rb
@@ -1,9 +1,10 @@
 require "rails_helper"
 
-RSpec.feature "Guest views protected case studies" do
+feature "Guest views protected case studies" do
   context "from the index page" do
     it "has a link to each case study" do
-      visit "/"
+      visit root_path
+
       within ".site-header" do
         click_link t("navigation.case_studies")
       end
@@ -18,9 +19,9 @@ RSpec.feature "Guest views protected case studies" do
 
   context "without a password" do
     it "and isn't allowed to view it" do
-      visit "/case_studies"
+      visit case_studies_path
 
-      click_link case_studies.first[0][:title]
+      click_on case_studies.first[0][:title]
 
       expect(page).to have_text t("lockup.lockup.unlock.heading")
     end
@@ -28,19 +29,15 @@ RSpec.feature "Guest views protected case studies" do
 
   context "with a password" do
     it "and is allowed to view it" do
-      visit "/case_studies"
+      visit case_studies_path
 
-      click_link case_studies.first[0][:title]
+      click_on case_studies.first[0][:title]
 
       fill_in "lockup_codeword", with: ENV['LOCKUP_CODEWORD']
       click_button t("lockup.lockup.unlock.submit")
 
       expect(page).to have_text case_studies.first[0][:title]
     end
-  end
-
-  def case_insensitive_string(string)
-    %r{#{string.humanize}}i
   end
 
   def case_studies
