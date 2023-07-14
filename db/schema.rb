4e2218892cbba694e37c2eeb1b0b13efa03a15b8
Jason Ramirez <jason@jasonramirez.com>
2023-07-14 13:11:53 -0500

    Add db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -78,17 +78,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_13_193545) do
     t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
-  create_table "posts_tags", id: false, force: :cascade do |t|
-    t.bigint "posts_id"
-    t.bigint "tags_id"
-    t.index ["posts_id"], name: "index_posts_tags_on_posts_id"
-    t.index ["tags_id"], name: "index_posts_tags_on_tags_id"
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
