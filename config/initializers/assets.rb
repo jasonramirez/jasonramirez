6f9e53b8a23a8a80002736082114dcac5f786838
Jason Ramirez <jason@jasonramirez.com>
2025-08-27 18:22:57 -0500

    Add audio asset path to initializer for improved audio file management.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "audio")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
@@ -23,5 +24,4 @@ Rails.application.config.assets.precompile += %w(
   jquery_ujs.js
   reading-progress-bar.js
   sky_labels.js
-  *.mp3
 )
