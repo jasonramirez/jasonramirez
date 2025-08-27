0065575f403869b90b0a98ee49597e86e7444c45
Jason Ramirez <jason@jasonramirez.com>
2025-08-27 18:59:07 -0500

    Add new audio files for Dropbox and Mayo Clinic, and update audio source paths in views to use relative paths for consistency.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,7 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.paths << Rails.root.join("app", "assets", "audio")
+Rails.application.config.assets.paths << Rails.root.join("app/assets/audios")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
@@ -24,4 +24,5 @@ Rails.application.config.assets.precompile += %w(
   jquery_ujs.js
   reading-progress-bar.js
   sky_labels.js
+  audios/*.mp3
 )
