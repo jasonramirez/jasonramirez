e12d45f983f367f94f1156310867bce046842c74
Jason Ramirez <jason@jasonramirez.com>
2025-08-27 21:36:59 -0500

    Add Intersession Experiences feature with video integration, new logo SVG, and updated styles for work links. Include video asset and enhance asset management for videos in initializer.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join("app/assets/audios")
+Rails.application.config.assets.paths << Rails.root.join("app/assets/videos")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
@@ -25,4 +26,5 @@ Rails.application.config.assets.precompile += %w(
   reading-progress-bar.js
   sky_labels.js
   audios/*.mp3
+  videos/*.mp4
 )
