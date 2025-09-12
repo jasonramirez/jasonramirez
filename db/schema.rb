6fc966e058df4602b973a8552dd688e4bd2cccb9
Jason Ramirez <jason@jasonramirez.com>
2025-09-11 21:50:38 -0500

    Remove pg_stat_statements extension from schema.rb as it is no longer needed for the current database setup.

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema[8.0].define(version: 2025_09_11_175123) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
-  enable_extension "pg_stat_statements"
   enable_extension "vector"
 
   create_table "admins", id: :serial, force: :cascade do |t|
