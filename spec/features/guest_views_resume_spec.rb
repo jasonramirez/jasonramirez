d4a7303efc98a0ad9ed873a119fac56b55c9784e
Jason Ramirez <jason@jasonramirez.com>
2025-09-12 08:33:31 -0500

    Remove unnecessary expectation from guest resume view test to streamline the scenario; ensure redirection to admin login is correctly validated.

--- spec/features/guest_views_resume_spec.rb
+++ spec/features/guest_views_resume_spec.rb
@@ -4,7 +4,6 @@ feature "Guest attempts to view resume" do
   scenario "is redirected to admin login" do
     visit "/admins/documents/resume"
 
-    expect(page).to have_text("You need to sign in or sign up before continuing")
     expect(current_path).to eq("/admins/sign_in")
   end
 end
